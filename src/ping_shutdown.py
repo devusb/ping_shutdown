c97c9922fbcba629e89a4d10dd508ab6af9fb32d
Morgan Helton <mhelton@superintendent.local>
2022-01-15 16:19:27 -0600

    add syslog for all output, use temp file to detect shutdown already in progress

--- src/ping_shutdown.py
+++ src/ping_shutdown.py
@@ -1,11 +1,12 @@
-from time import time
+from time import time,sleep
 from ping3 import ping
 from sys import argv
-from os import system
+from os import system,path,remove
 import syslog
 
-def shutdown_countdown(host,wait_time):
-    print('shutdown counter commencing...')
+def shutdown_countdown(host,wait_time,shutdown_cmd):
+    syslog.syslog('shutdown counter commencing...')
+    open('/tmp/shutdown_in_progress','a').close()
     start_time = time()
 
     while True:
@@ -15,27 +16,34 @@ def shutdown_countdown(host,wait_time):
         syslog.syslog(str(wait_time - elapsed_time) + " seconds until shutdown")
 
         if type(response) is float:
-            print('ping succeeded! shutdown aborted...')
+            syslog.syslog('ping succeeded! shutdown aborted...')
+            remove('/tmp/shutdown_in_progress')
             return
 
         if elapsed_time > wait_time:
-            print('shutting down...')
-            system('poweroff')
+            syslog.syslog('shutting down...')
+            remove('/tmp/shutdown_in_progress')
+            system(shutdown_cmd)
             return
 
 
 def main():
     host = argv[1]
     wait_time = int(argv[2])
+    shutdown_cmd = argv[3]
 
-    response = ping(host)
+    if not path.exists('/tmp/shutdown_in_progress'):
+        response = ping(host)
 
-    if response == False or response is None:
-        print('initial ping failed...')
-        shutdown_countdown(host,wait_time)
+        if response == False or response is None:
+            syslog.syslog('initial ping failed...')
+            shutdown_countdown(host,wait_time,shutdown_cmd)
 
+        else:
+            syslog.syslog('initial ping succeeded!')
+    
     else:
-        print('initial ping succeeded!')
+        syslog.syslog('shutdown already in progress')
 
 if __name__ == "__main__":
     main()
